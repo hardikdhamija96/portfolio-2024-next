f912f8bea4ddfa5b74f480d3946f5b790b6a374f
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-08-23 21:32:52 +0530

    finalizing color themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {
       backgroundImage: {
@@ -13,7 +14,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        lightBg : "rgba(var(--lightBg),1)",
+        darkBg : "rgba(var(--darkBg),1)",
+        darkBlue : "rgba(var(--darkBlue),1)",
+        babyBlue : "rgba(var(--babyBlue),1)",
+        midnightBlue : "rgba(var(--midnightBlue),1)",
+        babyBlue2 : "rgba(var(--babyBlue2),1)",
+        blueGrotto : "rgba(var(--blueGrotto),1)",
+        textColor : "rgba(var(--ivory),1)",
+        headingColor : "rgba(var(--mistyBlue),1)"
+      },
     },
+   
   },
   plugins: [],
 };
