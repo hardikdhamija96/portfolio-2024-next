6c799ac6ed618c24854d8a0eab5bce1d7f824c4b
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-18 14:36:23 +0530

    updated for mobile friendly - themeswitcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,18 +1,29 @@
 'use client'
 import { Icon } from '@iconify/react/dist/iconify.js'
 import { useTheme } from 'next-themes';
+import { useState } from 'react';
 
 const ThemeSwitcher = () => {
     const {theme, setTheme} = useTheme();
+    const [showThemes, setShowThemes] =useState()
     // const obj = useTheme();
     // console.log({obj});
+
+    const themeArray = ["gray","blue","dark","light"];
+
+    function changeTheme(){
+      let curr_ind = themeArray.indexOf(theme || "dark");
+      setTheme(themeArray[(curr_ind+1)%4]);
+    }
     
   return (
-    <div className='flex flex-row w-full justify-center items-center py-10 gap-6' >
-        <Icon className="text-headingColor text-[3.2rem] cursor-pointer" icon="jam:brightness" />
-        <div className='flex flex-row items-center gap-2 py-1 px-3 rounded-lg'>
+    <div className='flex flex-row w-full justify-center items-center py-10 gap-6 relative' >
+        
+        <Icon  className="text-headingColor text-[3.6rem] cursor-pointer animate-[spin_18s_linear_infinite]" icon="jam:brightness" onClick={changeTheme}></Icon>
+        
+        <div className='flex flex-row items-center sm:gap-2 gap-4 py-1 px-3 rounded-lg'>
             <div onClick={()=>setTheme("gray")} className={`size-6 bg-gray-500 rounded-full cursor-pointer ${theme === "gray" ? 'ring-1 ring-white':''}`} ></div>
-            <div onClick={()=>setTheme("blue")}  className={`size-6 bg-blue-700 rounded-full cursor-pointer ${theme === "blue" ? 'ring-1 ring-gray-800':''}`}></div>
+            <div onClick={()=>setTheme("blue")}  className={`size-6 bg-blue-700 rounded-full cursor-pointer ${theme === "blue" ? 'ring-1 ring-white':''}`}></div>
             <div onClick={()=>setTheme("dark")} className={`size-6 bg-gray-950 rounded-full cursor-pointer ${theme === "dark" ? 'ring-1 ring-white':''}`}></div>
             <div onClick={()=>setTheme("light")} className={`size-6 bg-gray-100 rounded-full cursor-pointer ${theme === "light" ? 'ring-1 ring-black':''}`}></div>
         </div>
