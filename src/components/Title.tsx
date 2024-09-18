ad79cb4f9037476facc939a29f6889f181ba59f9
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-18 14:35:08 +0530

    updated for mobile layout

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,7 +10,7 @@ interface TitleProps {
 function Title(props: TitleProps) {
   return (
     
-    <div className={`${lspartan.className} flex items-center justify-center text-[3.6rem]  font-bold text-headingColor active:text-[3.2rem] transition-all delay-[120] ease-in-out`}>
+    <div className={`${lspartan.className} flex items-center justify-center sm:text-[3.6rem] text-[4.6rem]  font-bold text-headingColor active:text-[3.2rem] transition-all delay-[120] ease-in-out`}>
         {props.name}
       </div>
   );
