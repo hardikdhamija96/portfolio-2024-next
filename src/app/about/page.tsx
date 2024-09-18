3de56207f619b962d4014d3c1df637b2dcaf460a
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-18 14:34:13 +0530

    added HTML charachter code for backtick

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,17 +16,17 @@ export default function AboutPage() {
       <div className="h-full max-w-7xl mx-auto flex flex-col justify-center items-center px-4">
         <p
           className={`${montSerrat.className} sm:text-xl text-headingColor pb-4 sm:mt-[16vh] mt-[4vh] text-left md:text-2xl`}>
-          Hi, I'm Hardik Dhamija, a <span className="font-semibold">Software Developer</span> specializing in <span className="font-semibold">React </span>
+          Hi, I&apos;m Hardik Dhamija, a <span className="font-semibold">Software Developer</span> specializing in <span className="font-semibold">React </span>
           with a growing interest in full-stack development. I create modern,
           responsive web applications using technologies such as React, Next.js,
-          and Node.js. I'm currently seeking opportunities to leverage my
+          and Node.js. I&apos;m currently seeking opportunities to leverage my
           frontend skills and further develop my backend expertise.
         </p>
         <p
           className={`${montSerrat.className} sm:text-xl text-left text-headingColor pb-4 md:text-2xl`}>
           I graduated from the <span className="font-semibold">Thapar Institute of Engineering & Technology</span>, a
           prestigious institution where I built a solid technical foundation.
-          Professionally, I’ve worked with <span className="font-semibold">TCS</span>, where I
+          Professionally, I&apos;ve worked with <span className="font-semibold">TCS</span>, where I
           focused on frontend development while gaining valuable experience with
           backend technologies. This combination has equipped me with a
           versatile skill set, allowing me to tackle both client and server-side
